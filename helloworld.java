f3e973ae8e4233558cb4f37391e8fea932ddb131
Amubavisri <153480103+Amubavisri@users.noreply.github.com>
2023-12-11 10:59:59 +0530

    Add files via upload

---
+++ helloworld.java
@@ -0,0 +1,8 @@
+//writing mt first program in java (HELLO WORLD)
+public class helloworld
+{
+       public static void main(String args[])
+       {
+                System.out.print("Hello world");
+        }
+}
