f072e360d20c7a51e1ff8e8bde3db51fe281e481
Amubavisri <153480103+Amubavisri@users.noreply.github.com>
2023-12-18 10:48:05 +0530

    Swapnumbers.java

--- Swapnumbers.java
+++ Swapnumbers.java
@@ -1,17 +1,17 @@
 import java.util.*;
 public class Swapnumbers
 {
-   public Static void main(String args[])
-  {
-    Scanner reader = new Scanner(System.in);
+   public static void main(String args[])
+      {
+          Scanner reader = new Scanner(System.in);
           System.out.print("\nEnter the first number: ");
-          int firstnum = reader.nextInt();    
+          int fn = reader.nextInt();    
           System.out.print("\nEnter the second number: ");
-          int secondnum = reader.nextInt();
+          int sn = reader.nextInt();
           reader.close();
-	  int temp = firstnum;
-          fistnum = secondnum;
-          secondnum = temp;
-          System.out.print("Swaped numbers are: First number = " + firstnum + " Second number: " + secondnum);    
+	  int temp = fn;
+          fn = sn;
+          sn = temp;
+          System.out.print("Swaped numbers are: First number = " + fn + " Second number: " + sn);    
       }
 }
